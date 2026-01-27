9ce606d4ebb050e45e8ef1eebb980915829dd4bd
Syed Ar Rafi <syed.ar.rafi@g.bracu.ac.com>
2026-01-27 08:14:04 +0600

    C# example of Do while loop or Do loop

---
+++ 01-Basics/Loops/DoWhileLoop/DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿// Do While Loop
+
+// It runs the codes inside the Do block at least one time. 
+// We need to add variable globally to use it later outside do code snippet or in while statement
+
+
+
+
+
+
+bool isValidAge;
+int age;
+
+
+do
+{
+    Console.Write("What is your age: ");
+    string ageText = Console.ReadLine();
+
+    isValidAge = int.TryParse(ageText, out age);
+    if (!isValidAge)
+    {
+        Console.WriteLine("That was an invalid age");
+    }
+}
+while (isValidAge ==false);
+Console.WriteLine($"You are {age} years old");
