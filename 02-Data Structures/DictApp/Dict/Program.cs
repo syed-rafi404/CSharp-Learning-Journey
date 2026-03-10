735c4873ac939826d0d7b9de656f5f0359ea6228
Syed Ar Rafi <syed.ar.rafi@g.bracu.ac.com>
2026-03-10 07:22:38 +0600

    Dictionary in C#

---
+++ 02-Data Structures/DictApp/Dict/Program.cs
@@ -0,0 +1,13 @@
+﻿// In dictionary the first parameter inside the <> is key and the second one is value.
+
+
+Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+
+keyValuePairs["animal"] = "Not Human";
+keyValuePairs["fish"] = "not something which can fly";
+keyValuePairs["human"] = "Smart Animal";
+ 
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[1] = "Syed Ar Rafi";
+employees[52] = "Clark Kent";
